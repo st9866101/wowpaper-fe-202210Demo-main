d304b2c23dec61313890b5c567d5f51bae1d4f8e
st9866101 <st9866101@gmail.com>
2022-10-27 11:42:32 +0800

    Updates

---
+++ precache-manifest.474ffbdc103610002bac82b827c3c684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "440ebd87ef62a5241061",
+    "url": "/static/css/main.d1c01ceb.chunk.css"
+  },
+  {
+    "revision": "440ebd87ef62a5241061",
+    "url": "/static/js/main.66ad151c.chunk.js"
+  },
+  {
+    "revision": "772d405df06fdcc60cb6",
+    "url": "/static/js/runtime~main.8bf700ab.js"
+  },
+  {
+    "revision": "14efdb1073c95d16d17d",
+    "url": "/static/css/2.756d3a9b.chunk.css"
+  },
+  {
+    "revision": "14efdb1073c95d16d17d",
+    "url": "/static/js/2.8e2834c6.chunk.js"
+  },
+  {
+    "revision": "c832614f3909c6f83d95",
+    "url": "/static/js/3.41c7b25b.chunk.js"
+  },
+  {
+    "revision": "66b0608fa1da8565865dded785618683",
+    "url": "/index.html"
+  }
+];
